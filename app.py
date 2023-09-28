aff937bdaefbb1f9b370258f5735c1c6ea44d63d
bfpbr5 <bffpbr5@gmail.com>
2023-09-28 20:40:04 +0800

    remove github

--- app.py
+++ app.py
@@ -15,6 +15,18 @@ openai = promptlayer.openai
 st.set_page_config(page_title='Digital Aurora Assistant', layout='wide', page_icon='🤖')
 # 自定义元素样式
 st.markdown(css_code, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 if "initial_settings" not in st.session_state:
     # 历史聊天窗口
